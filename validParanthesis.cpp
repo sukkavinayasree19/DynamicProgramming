5ee27a9da6fbfc539da25ebf88574cd29f432e0b
Sukka vinaya sree <54493866+sukkavinayasree19@users.noreply.github.com>
2020-06-03 19:14:38 +0530

    Create validParanthesis.cpp
    To check how many parenthesis are valid.

---
+++ validParanthesis.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include<string>
+using namespace std;
+const int n=1e5+4;
+int dp[n];
+int main() {
+	// your code goes here
+	string s;int j,i;
+	cin>>s;int res=0;
+	for(i=0;i<s.length();i++)
+	{
+		if(s[i]=='(')
+		{
+			dp[i+1]=0;
+		}
+		else
+		{
+			j=i-dp[i]-1;
+			if(s[j]=='(')
+			dp[i+1]=dp[i-1]+dp[j]+2;
+			else
+			dp[i+1]=0;
+		}
+		res=max(res,dp[i+1]);
+	}
+	cout<<res;
+	return 0;
+}
